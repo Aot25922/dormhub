2e9e45523d85447acce946424daeee759b9e74d4
thanasapL <thanasap.lee@outlook.com>
2022-04-14 13:48:56 +0700

    [Update] edit relation db

--- init.sql
+++ init.sql
@@ -1,140 +1,73 @@
 DROP SCHEMA IF EXISTS `int365_dormhub` ;
 CREATE SCHEMA IF NOT EXISTS `int365_dormhub` DEFAULT CHARACTER SET utf8 ;
 USE int365_dormhub;
-DROP TABLE IF EXISTS `blog`;
-DROP TABLE IF EXISTS `user`;
-DROP TABLE IF EXISTS `paymentMethod`;
-DROP TABLE IF EXISTS `agreement`;
-DROP TABLE IF EXISTS `banking`;
-DROP TABLE IF EXISTS `source`;
-DROP TABLE IF EXISTS `roomService`;
-DROP TABLE IF EXISTS `service`;
+DROP TABLE IF EXISTS `media`;
+DROP TABLE IF EXISTS `roomDetail`;
+DROP TABLE IF EXISTS `detail`;
 DROP TABLE IF EXISTS `room`;
 DROP TABLE IF EXISTS `roomType`;
+DROP TABLE IF EXISTS `bankAccount`;
+DROP TABLE IF EXISTS `bank`;
 DROP TABLE IF EXISTS `dorm`;
+DROP TABLE IF EXISTS `booking`;
+DROP TABLE IF EXISTS `userAccount`;
 DROP TABLE IF EXISTS `address`;
-DROP TABLE IF EXISTS `addrDetail`;
-DROP TABLE IF EXISTS `location`;
+DROP TABLE IF EXISTS `subDistrict`;
+DROP TABLE IF EXISTS `zipCode`;
+DROP TABLE IF EXISTS `district`;
+DROP TABLE IF EXISTS `province`;
+DROP TABLE IF EXISTS `region`;
 
-CREATE TABLE `location` (
-	locationId		CHAR(5)				NOT NULL,
-    city			VARCHAR(50)			NOT NULL,
-    region			VARCHAR(50)			NOT NULL,
-    country			VARCHAR(30)			NOT NULL,
-    img				VARCHAR(1000)		NOT NULL,
-    CONSTRAINT locationId_pk PRIMARY KEY ( locationId )
+CREATE TABLE `region` (
+	regionId		CHAR(1)				NOT NULL,
+    name			VARCHAR(50)			NOT NULL,
+    CONSTRAINT regionId_pk PRIMARY KEY ( regionId )
 );
 
-CREATE TABLE `addrDetail` (
-    zipCode			CHAR(5)				NOT NULL,
-    state			VARCHAR(30)			NOT NULL,
-    district		VARCHAR(30)			NOT NULL,
-    locationId		CHAR(5)				NOT NULL,
-    CONSTRAINT zipCode_pk PRIMARY KEY ( zipCode ),
-    CONSTRAINT locationId_fk FOREIGN KEY ( locationId ) REFERENCES `location` ( locationId )
+CREATE TABLE `province` (
+    provinceId		VARCHAR(2)			NOT NULL,
+    name			VARCHAR(50)			NOT NULL,
+    img				VARCHAR(1000)		NOT NULL,
+    regionId		CHAR(1)				NOT NULL,
+    CONSTRAINT provinceId_pk PRIMARY KEY ( provinceId ),
+    CONSTRAINT regionId_fk FOREIGN KEY ( regionId ) REFERENCES `region` ( regionId )
+);
+
+CREATE TABLE `district` (
+    districtId		VARCHAR(4)			NOT NULL,
+    name			VARCHAR(50)			NOT NULL,
+    provinceId		VARCHAR(2)			NOT NULL,
+    CONSTRAINT districtId_pk PRIMARY KEY ( districtId ),
+    CONSTRAINT provinceId_fk FOREIGN KEY ( provinceId ) REFERENCES `province` ( provinceId )
+);
+
+CREATE TABLE `zipCode` (
+    zipCodeId		CHAR(5)			NOT NULL,
+    CONSTRAINT zipCodeId_pk PRIMARY KEY ( zipCodeId )
+);
+
+CREATE TABLE `subDistrict` (
+    subDistrictId	VARCHAR(4)			NOT NULL,
+    name			VARCHAR(50)			NOT NULL,
+    districtId		VARCHAR(4)			NOT NULL,
+    zipCodeId		CHAR(5)				NOT NULL,
+    CONSTRAINT subDistrictId_pk PRIMARY KEY ( subDistrictId ),
+    CONSTRAINT districtId_fk FOREIGN KEY ( districtId ) REFERENCES `district` ( districtId ),
+    CONSTRAINT zipCodeId_fk FOREIGN KEY ( zipCodeId ) REFERENCES `zipCode` ( zipCodeId )
 );
 
 CREATE TABLE `address` (
-	addressId		CHAR(6)				NOT NULL,
+	addressId		VARCHAR(6)				NOT NULL,
     number			VARCHAR(20)			NOT NULL,
     street			VARCHAR(30)			NOT NULL,
     alley			VARCHAR(5),
-    zipCode			CHAR(5)				NOT NULL,
-    CONSTRAINT addressDorm_pk PRIMARY KEY ( addressId ),
-    CONSTRAINT zipCode_fk FOREIGN KEY ( zipCode ) REFERENCES `addrDetail` ( zipCode )
+    subDistrictId	VARCHAR(4)			NOT NULL,
+    CONSTRAINT addressId_pk PRIMARY KEY ( addressId ),
+    CONSTRAINT subDistrictId_addr_fk FOREIGN KEY ( subDistrictId ) REFERENCES `subDistrict` ( subDistrictId )
 );
 
-CREATE TABLE `dorm` (
-	dormId			CHAR(5)				NOT NULL,
-	name			VARCHAR(50)			NOT NULL,
-    openTime		TIME,
-    closeTime 		TIME,
-    description 	VARCHAR(200),
-    bookingTimeout	INT					NOT NULL,
-    status			VARCHAR(20)			NOT NULL,
-    rate			DECIMAL(3, 2)		NOT NULL,
-    addressId		CHAR(6)				NOT NULL,
-    CONSTRAINT dormId_pk PRIMARY KEY ( dormId ),
-     CONSTRAINT addressId_dorm_fk FOREIGN KEY ( addressId ) REFERENCES `address` ( addressId )
-);
-
-CREATE TABLE `roomType` (
-	roomTypeId		CHAR(5)				NOT NULL,
-    type			VARCHAR(50)			NOT NULL,
-    CONSTRAINT roomTypeId_pk PRIMARY KEY ( roomTypeId )
-);
-
-CREATE TABLE `room` (
-	roomId			CHAR(5)				NOT NULL,
-    roomNum			VARCHAR(50)			NOT NULL,
-    status			VARCHAR(120)		NOT NULL,
-    floors			INT					NOT NULL,
-    price			INT					NOT NULL,
-    description		VARCHAR(200),
-    dormId			CHAR(5)				NOT NULL,
-    roomTypeId		CHAR(5)				NOT NULL,
-    CONSTRAINT roomId_pk PRIMARY KEY ( roomId ),
-    CONSTRAINT dormId_room_fk FOREIGN KEY ( dormId ) REFERENCES `dorm` ( dormId ),
-    CONSTRAINT roomTypeId_room_fk FOREIGN KEY ( roomTypeId ) REFERENCES `roomType` ( roomTypeId )
-);
-
-CREATE TABLE `service` (
-	serviceId		CHAR(5)				NOT NULL,
-    name			VARCHAR(50)			NOT NULL,
-    description		VARCHAR(200)		NOT NULL,
-    CONSTRAINT serviceId_pk PRIMARY KEY ( serviceId )
-);
-
-CREATE TABLE `roomService` (
-	roomTypeId		CHAR(5)				NOT NULL,
-    serviceId		CHAR(5)				NOT NULL,
-    CONSTRAINT rsId_pk PRIMARY KEY ( roomTypeId, serviceId ),
-    CONSTRAINT roomTypeId_fk FOREIGN KEY ( roomTypeId ) REFERENCES `roomType` ( roomTypeId ),
-    CONSTRAINT serviceId_fk FOREIGN KEY ( serviceId ) REFERENCES `service` ( serviceId )
-);
-
-CREATE TABLE `source` (
-	sourceId		CHAR(5)				NOT NULL,
-	path			VARCHAR(1000)		NOT NULL,
-    name			VARCHAR(50),
-    dormId			CHAR(5)				NOT NULL,
-    roomTypeId		CHAR(5),
-    CONSTRAINT sourceId_pk PRIMARY KEY ( sourceId ),
-    CONSTRAINT dormId_source_fk FOREIGN KEY ( dormId ) REFERENCES `dorm` ( dormId ),
-    CONSTRAINT roomTypeId_source_fk FOREIGN KEY ( roomTypeId ) REFERENCES `roomType` ( roomTypeId )
-);
-
-CREATE TABLE `banking` (
-	bankId			CHAR(3)				NOT NULL,
-    logo			VARCHAR(1000)		NOT NULL,
-    name			VARCHAR(50)			NOT NULL,
-    CONSTRAINT bankId PRIMARY KEY ( bankId )
-);
-
-CREATE TABLE `agreement` (
-	agreementId		CHAR(6)				NOT NULL,
-	amount			DECIMAL(6, 2)		NOT NULL,
-	payDate			DATETIME			NOT NULL,
-    startDate		DATETIME			NOT NULL,
-    endDate			DATETIME			NOT NULL,
-    status			CHAR(10)			NOT NULL,
-    description		VARCHAR(120),
-    CONSTRAINT agreementId_pk PRIMARY KEY ( agreementId )
-);
-
-CREATE TABLE `paymentMethod` (
-	methodId		CHAR(5)				NOT NULL,
-    accountNum		CHAR(10)			NOT NULL,
-    dormId			CHAR(5)				NOT NULL,
-    bankId			CHAR(3)				NOT NULL,
-    agreementId		CHAR(6)				NOT NULL,
-    CONSTRAiNT methodId_pk PRIMARY KEY ( methodId ),
-    CONSTRAINT dormId_fk FOREIGN KEY ( dormId ) REFERENCES `dorm` ( dormId ),
-    CONSTRAINT bankId_fk FOREIGN KEY ( bankId ) REFERENCES `banking` ( bankId ),
-    CONSTRAINT agreementId_fk FOREIGN KEY ( agreementId ) REFERENCES `agreement` ( agreementId )
-);
-
-CREATE TABLE `user` (
+CREATE TABLE `userAccount` (
+	userId			VARCHAR(5)			NOT NULL,
 	email			VARCHAR(50)			NOT NULL,
     password		VARCHAR(100)		NOT NULL,
     fname			VARCHAR(50)			NOT NULL,
@@ -142,20 +75,103 @@ CREATE TABLE `user` (
     sex				VARCHAR(20)			NOT NULL,
     phone			CHAR(10)			NOT NULL,
     role			VARCHAR(20)			NOT NULL,
-    addressId		VARCHAR(10)			NOT NULL,
-    agreementId		CHAR(6),
-    CONSTRAINT email_pk PRIMARY KEY ( email ),
-    CONSTRAINT phone_uc UNIQUE (phone),
-    CONSTRAINT addressId_user_fk FOREIGN KEY ( addressId ) REFERENCES `address` ( addressId ),
-    CONSTRAINT agreementId_user_fk FOREIGN KEY ( agreementId ) REFERENCES `agreement` ( agreementId )
+    addressId		VARCHAR(10),
+    CONSTRAINT userId_pk PRIMARY KEY ( userId ),
+    CONSTRAINT contact_uc UNIQUE (phone, email),
+    CONSTRAINT addressId_user_fk FOREIGN KEY ( addressId ) REFERENCES `address` ( addressId )
 );
 
-CREATE TABLE `blog` (
-	blogId			CHAR(5)				NOT NULL,
-    description		VARCHAR(120)		NOT NULL,
-    email			VARCHAR(50)			NOT NULL,
-	dormId			CHAR(5)				NOT NULL,
-    CONSTRAINT blogId_pk PRIMARY KEY ( blogId ),
-    CONSTRAINT email_fk FOREIGN KEY ( email ) REFERENCES `user` ( email ),
-    CONSTRAINT dormId_blog_fk FOREIGN KEY ( dormId ) REFERENCES `dorm` ( dormId )
+CREATE TABLE `booking` (
+	bookingId		VARCHAR(6)			NOT NULL,
+	payDate			DATETIME			NOT NULL,
+    startDate		DATETIME			NOT NULL,
+    endDate			DATETIME			NOT NULL,
+    status			VARCHAR(10)			NOT NULL,
+    description		VARCHAR(120),
+    userId			VARCHAR(5),
+    CONSTRAINT bookingId_pk PRIMARY KEY ( bookingId ),
+    CONSTRAINT userId_fk FOREIGN KEY ( userId ) REFERENCES `userAccount` ( userId )
+);
+
+CREATE TABLE `dorm` (
+	dormId			VARCHAR(5)			NOT NULL,
+	name			VARCHAR(50)			NOT NULL,
+    openTime		TIME,
+    closeTime 		TIME,
+    description 	VARCHAR(200),
+    rating			DECIMAL(3, 2)		NOT NULL,
+    acceptPercent	DECIMAL(5, 2)		NOT NULL,
+    elecPerUnit		DECIMAL(3, 2)		NOT NULL,
+    waterPerUnit	DECIMAL(3, 2)		NOT NULL,
+    addressId		VARCHAR(5)			NOT NULL,
+    ownerId			VARCHAR(5)			NOT NULl,
+    CONSTRAINT dorm_pk PRIMARY KEY ( dormId, addressId ),
+	CONSTRAINT addressId_dorm_fk FOREIGN KEY ( addressId ) REFERENCES `address` ( addressId ),
+	CONSTRAINT ownerId_fk FOREIGN KEY ( ownerId ) REFERENCES `userAccount` ( userId )
+);
+
+CREATE TABLE `bank` (
+	bankId			VARCHAR(3)			NOT NULL,
+    name			VARCHAR(30)			NOT NULL,
+    logo			VARCHAR(1000)		NOT NULL,
+    CONSTRAINT bankId PRIMARY KEY ( bankId )
+);
+
+CREATE TABLE `bankAccount` (
+	bankAccId		VARCHAR(3)			NOT NULL,
+    accountNum		CHAR(10)			NOT NULL,
+    qrCode			VARCHAR(1000),
+    dormId			VARCHAR(5)			NOT NULL,
+    bankId			VARCHAR(3)			NOT NULL,
+    CONSTRAINT bankId PRIMARY KEY ( bankId ),
+    CONSTRAINT dormId_bankAcc_fk FOREIGN KEY ( dormId ) REFERENCES `dorm` ( dormId ),
+    CONSTRAINT bankId_fk FOREIGN KEY ( bankId ) REFERENCES `bank` ( bankId )
+);
+
+CREATE TABLE `roomType` (
+	roomTypeId		VARCHAR(2)			NOT NULL,
+    type			VARCHAR(50)			NOT NULL,
+    CONSTRAINT roomTypeId_pk PRIMARY KEY ( roomTypeId )
+);
+
+CREATE TABLE `room` (
+	roomId			VARCHAR(5)			NOT NULL,
+    roomNum			VARCHAR(50)			NOT NULL,
+    status			VARCHAR(10)			NOT NULL,
+    floors			INT					NOT NULL,
+    price			INT					NOT NULL,
+    description		VARCHAR(200),
+    deposit			INT					NOT NULL,
+    dormId			VARCHAR(5)			NOT NULL,
+    roomTypeId		VARCHAR(5)			NOT NULL,
+    bookingId		VARCHAR(6),
+    CONSTRAINT roomId_pk PRIMARY KEY ( roomId ),
+    CONSTRAINT dormId_room_fk FOREIGN KEY ( dormId ) REFERENCES `dorm` ( dormId ),
+    CONSTRAINT roomTypeId_room_fk FOREIGN KEY ( roomTypeId ) REFERENCES `roomType` ( roomTypeId ),
+    CONSTRAINT bookingId_fk FOREIGN KEY ( bookingId ) REFERENCES `booking` ( bookingId )
+);
+
+CREATE TABLE `detail` (
+	detailId		VARCHAR(5)			NOT NULL,
+    name			VARCHAR(50)			NOT NULL,
+    description		VARCHAR(200)		NOT NULL,
+    CONSTRAINT detailId_pk PRIMARY KEY ( detailId )
+);
+
+CREATE TABLE `roomDetail` (
+	roomTypeId		VARCHAR(2)				NOT NULL,
+    detailId		VARCHAR(5)				NOT NULL,
+    CONSTRAINT roomTypeId_fk FOREIGN KEY ( roomTypeId ) REFERENCES `roomType` ( roomTypeId ),
+    CONSTRAINT detailId_fk FOREIGN KEY ( detailId ) REFERENCES `detail` ( detailId )
+);
+
+CREATE TABLE `media` (
+	mediaId			VARCHAR(5)			NOT NULL,
+	path			VARCHAR(1000)		NOT NULL,
+    name			VARCHAR(50),
+    dormId			CHAR(5)				NOT NULL,
+    roomTypeId		CHAR(5),
+    CONSTRAINT mediaId_pk PRIMARY KEY ( mediaId ),
+    CONSTRAINT dormId_md_fk FOREIGN KEY ( dormId ) REFERENCES `dorm` ( dormId ),
+    CONSTRAINT roomTypeId_md_fk FOREIGN KEY ( roomTypeId ) REFERENCES `roomType` ( roomTypeId )
 );
--- insert.sql
+++ insert.sql
@@ -1,35 +1,45 @@
-INSERT INTO `location`(locationId, city, region, country, img) VALUES ('LC001', 'Bangkok', 'Center', 'Thailand', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fbrandinside.asia%2Fbangkok-global-destination-city-of-the-world%2F&psig=AOvVaw3F1V2JO070BfwN9iR-LzZ0&ust=1649741940858000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCKC4iOili_cCFQAAAAAdAAAAABAD');
-INSERT INTO `location`(locationId, city, region, country, img) VALUES ('LC002', 'Changmai', 'North', 'Thailand', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fth.hotels.com%2Fgo%2Fthailand%2Fchiang-mai&psig=AOvVaw3xMi7P1mZxNxJk9uxTC_vL&ust=1649741963727000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCIDTrfGli_cCFQAAAAAdAAAAABAD');
+INSERT INTO `region`(regionId, name) VALUES ('1', 'Center');
+INSERT INTO `region`(regionId, name) VALUES ('2', 'North');
 
-INSERT INTO `addrDetail`(zipCode, state, district, locationId) VALUES ('SD001', 'state1', 'district1', 'LC001');
-INSERT INTO `addrDetail`(zipCode, state, district, locationId) VALUES ('SD002', 'state2', 'district2', 'LC002');
+INSERT INTO `province`(provinceId, name, img, regionId) VALUES ('1', 'Bangkok', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fbrandinside.asia%2Fbangkok-global-destination-city-of-the-world%2F&psig=AOvVaw3F1V2JO070BfwN9iR-LzZ0&ust=1649741940858000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCKC4iOili_cCFQAAAAAdAAAAABAD', '1');
+INSERT INTO `province`(provinceId, name, img, regionId) VALUES ('2', 'Changmai', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fth.hotels.com%2Fgo%2Fthailand%2Fchiang-mai&psig=AOvVaw3xMi7P1mZxNxJk9uxTC_vL&ust=1649741963727000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCIDTrfGli_cCFQAAAAAdAAAAABAD', '2');
 
-INSERT INTO `address`(addressId, number, street, alley, zipCode) VALUES ('AD0001', '192/168', 'St.Road1', null, 'SD001');
-INSERT INTO `address`(addressId, number, street, alley, zipCode) VALUES ('AD0002', '1/168', 'St.Road2', null, 'SD002');
+INSERT INTO `district`(districtId, name, provinceId) VALUES ('1', 'state1', '1');
+INSERT INTO `district`(districtId, name, provinceId) VALUES ('2', 'state2', '2');
 
-INSERT INTO `dorm`(dormId, name, opentime, closeTime, description, bookingTimeout, status, rate, addressId) VALUES ('D0001', 'SUSURUN', null, null, null, 3,'Verified', 4.25, 'AD0001');
-INSERT INTO `dorm`(dormId, name, opentime, closeTime, description, bookingTimeout, status, rate, addressId) VALUES ('D0002', 'Neoarmstrong Canon', null, null, null, 4,'Verified', 5.00, 'AD0002');
+INSERT INTO `zipCode`(zipCodeId) VALUES ('10120');
+INSERT INTO `zipCode`(zipCodeId) VALUES ('10130');
 
-INSERT INTO `roomType`(roomTypeId, type) VALUES ('RT001', 'Small');
-INSERT INTO `roomType`(roomTypeId, type) VALUES ('RT002', 'Big');
+INSERT INTO `subDistrict`(subDistrictId, name, districtId, zipCodeId) VALUES ('1', 'subDistrict1', '1','10120');
+INSERT INTO `subDistrict`(subDistrictId, name, districtId, zipCodeId) VALUES ('2', 'subDistrict2', '2','10130');
 
-INSERT INTO `room`(roomId, roomNum, status, floors, price, description, dormId, roomTypeId) VALUES ('R0001', '101', 'Full', 1, 2500, null, 'D0001', 'RT001');
-INSERT INTO `room`(roomId, roomNum, status, floors, price, description, dormId, roomTypeId) VALUES ('R0002', '102', 'Idle', 1, 2500, null, 'D0001', 'RT002');
-INSERT INTO `room`(roomId, roomNum, status, floors, price, description, dormId, roomTypeId) VALUES ('R0003', '102', 'Idle', 1, 2500, null, 'D0002', 'RT001');
+INSERT INTO `address`(addressId, number, street, alley, subDistrictId) VALUES ('1', '192/168', 'St.Road1', null, '1');
+INSERT INTO `address`(addressId, number, street, alley, subDistrictId) VALUES ('2', '1/168', 'St.Road2', null, '2');
 
-INSERT INTO `source`(sourceId, path, name, dormId, roomTypeId) VALUES ('S0001', 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/549e9b77-4c90-4c7f-8d0e-772a4ba70576/d9tyf2k-81e3e921-c712-4e0e-88b8-c3169a910d93.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU0OWU5Yjc3LTRjOTAtNGM3Zi04ZDBlLTc3MmE0YmE3MDU3NlwvZDl0eWYyay04MWUzZTkyMS1jNzEyLTRlMGUtODhiOC1jMzE2OWE5MTBkOTMuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.l1dqqj8cs7o23NqsHxRygLJMalH5M3DCcazHyS_9V1k', null, 'D0001', null);
-INSERT INTO `source`(sourceId, path, name, dormId, roomTypeId) VALUES ('S0002', 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/287eab66-0f73-4b97-8e74-849c5f06d542/ddrinx9-2e0cfe76-97c0-4008-8382-990d9f6f5af5.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI4N2VhYjY2LTBmNzMtNGI5Ny04ZTc0LTg0OWM1ZjA2ZDU0MlwvZGRyaW54OS0yZTBjZmU3Ni05N2MwLTQwMDgtODM4Mi05OTBkOWY2ZjVhZjUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.SEU1kEIhjs4yhvyIYj7XLMOgyk47lkrK65cbsTXcDMk', null, 'D0002', null);
-INSERT INTO `source`(sourceId, path, name, dormId, roomTypeId) VALUES ('S0003', 'http://f.ptcdn.info/189/029/000/1425962771-gintama216-o.jpg', null, 'D0001', 'RT001');
-INSERT INTO `source`(sourceId, path, name, dormId, roomTypeId) VALUES ('S0004', 'https://f.ptcdn.info/191/029/000/1425968149-2z3orpv-o.jpg', null, 'D0001', 'RT001');
-INSERT INTO `source`(sourceId, path, name, dormId, roomTypeId) VALUES ('S0005', 'https://cdnmetv.metv.com/WzeFS-1619719775-16228-list_items-bugs_sunglasses.jpg', null, 'D0001', 'RT001');
-INSERT INTO `source`(sourceId, path, name, dormId, roomTypeId) VALUES ('S0006', 'https://cdnmetv.metv.com/AO512-1619719640-16224-list_items-chungus.jpg', null, 'D0001', 'RT001');
-INSERT INTO `source`(sourceId, path, name, dormId, roomTypeId) VALUES ('S0007', 'https://cdnmetv.metv.com/A7Uev-1619719796-16229-list_items-bugs_wish.jpg', null, 'D0001', 'RT002');
-INSERT INTO `source`(sourceId, path, name, dormId, roomTypeId) VALUES ('S0008', 'https://cdnmetv.metv.com/z50xp-1619719725-16226-list_items-no.jpg', null, 'D0001', 'RT002');
-INSERT INTO `source`(sourceId, path, name, dormId, roomTypeId) VALUES ('S0009', 'https://cdnmetv.metv.com/AGKJm-1619719750-16227-list_items-bugs_gun.jpg', null, 'D0002', 'RT001');
-INSERT INTO `source`(sourceId, path, name, dormId, roomTypeId) VALUES ('S0010', 'https://cdnmetv.metv.com/Vpf7q-1619719679-16225-list_items-bugs-commy.jpg', null, 'D0002', 'RT001');
+INSERT INTO `userAccount`(userId, email, password, fname, lname, sex, phone, role, addressId) VALUES ('1', 'dorm1@mail.com', 'test', 'Fname1', 'Lname1', 'Male', '1234567890', 'Dorm Manage', '1');
+INSERT INTO `userAccount`(userId, email, password, fname, lname, sex, phone, role, addressId) VALUES ('2', 'dorm2@mail.com', 'test', 'Fname2', 'Lname2', 'Female', '1234567899', 'Dorm Manage', '2');
+INSERT INTO `userAccount`(userId, email, password, fname, lname, sex, phone, role, addressId) VALUES ('3', 'dorm3@mail.com', 'test', 'Fname3', 'Lname3', 'Female', '1234567879', 'User', null);
 
-INSERT INTO `user`(email, password, fname, lname, sex, phone, role, addressId, agreementId) VALUES ('dorm1@mail.com', 'test', 'Dorm1', 'Lname1', 'Male', '1234567890', 'Dorm Manage', 'AD0001', null);
-INSERT INTO `user`(email, password, fname, lname, sex, phone, role, addressId, agreementId) VALUES ('dorm2@mail.com', 'test', 'Dorm2', 'Lname2', 'Female', '1234567899', 'Dorm Manage', 'AD0002', null);
+INSERT INTO `dorm`(dormId, name, opentime, closeTime, description, rating, acceptPercent, elecPerUnit, waterPerUnit, addressId, ownerId) VALUES ('1', 'SUSURUN', null, null, null, 3.25, 90.00, 3.25, 1.23,'1', '1');
+INSERT INTO `dorm`(dormId, name, opentime, closeTime, description, rating, acceptPercent, elecPerUnit, waterPerUnit, addressId, ownerId) VALUES ('2', 'Neoarmstrong Canon', null, null, null, 4.33, 72.26, 4.00, 2.01, '2', '2');
+
+INSERT INTO `roomType`(roomTypeId, type) VALUES ('1', 'Small');
+INSERT INTO `roomType`(roomTypeId, type) VALUES ('2', 'Big');
+
+INSERT INTO `room`(roomId, roomNum, status, floors, price, description, deposit, dormId, roomTypeId) VALUES ('1', '101', 'Full', 1, 2500, null, 1050,'1', '1');
+INSERT INTO `room`(roomId, roomNum, status, floors, price, description, deposit, dormId, roomTypeId) VALUES ('2', '102', 'Idle', 1, 2800, null, 1200,'1', '2');
+INSERT INTO `room`(roomId, roomNum, status, floors, price, description, deposit, dormId, roomTypeId) VALUES ('3', '101', 'Idle', 1, 2500, null, 1100,'2', '1');
+
+INSERT INTO `media`(mediaId, path, name, dormId, roomTypeId) VALUES ('1', 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/549e9b77-4c90-4c7f-8d0e-772a4ba70576/d9tyf2k-81e3e921-c712-4e0e-88b8-c3169a910d93.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU0OWU5Yjc3LTRjOTAtNGM3Zi04ZDBlLTc3MmE0YmE3MDU3NlwvZDl0eWYyay04MWUzZTkyMS1jNzEyLTRlMGUtODhiOC1jMzE2OWE5MTBkOTMuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.l1dqqj8cs7o23NqsHxRygLJMalH5M3DCcazHyS_9V1k', null, '1', null);
+INSERT INTO `media`(mediaId, path, name, dormId, roomTypeId) VALUES ('2', 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/287eab66-0f73-4b97-8e74-849c5f06d542/ddrinx9-2e0cfe76-97c0-4008-8382-990d9f6f5af5.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI4N2VhYjY2LTBmNzMtNGI5Ny04ZTc0LTg0OWM1ZjA2ZDU0MlwvZGRyaW54OS0yZTBjZmU3Ni05N2MwLTQwMDgtODM4Mi05OTBkOWY2ZjVhZjUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.SEU1kEIhjs4yhvyIYj7XLMOgyk47lkrK65cbsTXcDMk', null, '2', null);
+INSERT INTO `media`(mediaId, path, name, dormId, roomTypeId) VALUES ('3', 'http://f.ptcdn.info/189/029/000/1425962771-gintama216-o.jpg', null, '1', '1');
+INSERT INTO `media`(mediaId, path, name, dormId, roomTypeId) VALUES ('4', 'https://f.ptcdn.info/191/029/000/1425968149-2z3orpv-o.jpg', null, '1', '1');
+INSERT INTO `media`(mediaId, path, name, dormId, roomTypeId) VALUES ('5', 'https://cdnmetv.metv.com/WzeFS-1619719775-16228-list_items-bugs_sunglasses.jpg', null, '1', '1');
+INSERT INTO `media`(mediaId, path, name, dormId, roomTypeId) VALUES ('6', 'https://cdnmetv.metv.com/AO512-1619719640-16224-list_items-chungus.jpg', null, '1', '1');
+INSERT INTO `media`(mediaId, path, name, dormId, roomTypeId) VALUES ('7', 'https://cdnmetv.metv.com/A7Uev-1619719796-16229-list_items-bugs_wish.jpg', null, '1', '2');
+INSERT INTO `media`(mediaId, path, name, dormId, roomTypeId) VALUES ('8', 'https://cdnmetv.metv.com/z50xp-1619719725-16226-list_items-no.jpg', null, '1', '2');
+INSERT INTO `media`(mediaId, path, name, dormId, roomTypeId) VALUES ('9', 'https://cdnmetv.metv.com/AGKJm-1619719750-16227-list_items-bugs_gun.jpg', null, '2', '1');
+INSERT INTO `media`(mediaId, path, name, dormId, roomTypeId) VALUES ('10', 'https://cdnmetv.metv.com/Vpf7q-1619719679-16225-list_items-bugs-commy.jpg', null, '2', '1');
 
 -- SELECT * FROM int365_dormhub.address;
 -- SELECT * FROM int365_dormhub.region;
